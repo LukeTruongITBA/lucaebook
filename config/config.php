bf48faf9d5834e09f42bc0286bcdadf05925eede
Truong Tan Loc <truongtanlocbf.forwork@gmail.com>
2025-12-03 17:57:25 +0700

    chore: Adjust whitespace in config

--- config/config.php
+++ config/config.php
@@ -22,7 +22,7 @@
 
     $secret_key = "sk_test_51M94h5Hp65tXrQ3P4wJRttxoeJDyxOXhUc1p402Oc4fbmtzTK1VZ3yDF5kJKIty9Sf1ygA7WDevSht8Ba88SbSsk00GwlnUHF9";
 
-    
+
 
     // if($conn) {
     //     echo "worked successfully";
